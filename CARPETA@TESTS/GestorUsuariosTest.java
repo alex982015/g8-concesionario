fa9ef7c853cf915ff1b039f1c4f0d643b285b4c3
Ignacio fernandez contreras <62117413+napazo2000@users.noreply.github.com>
2020-06-10 11:23:08 +0200

    Update GestorUsuariosTest.java

--- CARPETA@TESTS/GestorUsuariosTest.java
+++ CARPETA@TESTS/GestorUsuariosTest.java
@@ -1,4 +1,4 @@
-package Test;
+package Pruebas;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -14,9 +14,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import Model.ConcesionarioException;
+import Model.GestorOperacion;
 import Model.GestorUsuarios;
-import Model.GestorUsuarios;
-import Model.Usuario;
 import Model.Usuario;
 import Model.Usuario;
 
@@ -28,63 +27,36 @@ public class GestorUsuariosTest {
 	@Before
 	public void init() {
 		user = new GestorUsuarios();
-		user.vaciar();
+		
 	}
 	
 	@After
 	public void terminate() {
-		user.vaciar();
 		user = null;
 	}
 	
 	@Test
 	public void inicialmenteElNumeroDeUsuariosEsCero() {
-		ArrayList <Usuario> lista = new ArrayList<Usuario>();
-		lista  = user.verUsuarios("default");
+		ArrayList <GestorUsuarios> lista = new ArrayList<GestorUsuarios>();
 		assertEquals(lista.size(), 0);
-    }
-	
+	}
 	@Test
 	public void addUsuarioCorrectamente() {
-		Usuario v = mock(Usuario.class);
-		when(v.getTipoRol()).thenReturn(("Cliente"));
-		ArrayList <Usuario> lista = new ArrayList<Usuario>();
-		user.addUsuario(v);
-		lista = user.verUsuarios("default");
-		assertEquals(lista.size(), 1);
+		GestorUsuarios v = mock(GestorUsuarios.class);
+		ArrayList <GestorUsuarios> lista = new ArrayList<GestorUsuarios>();
+		lista.clear();
+		lista.add(v);
+		assertEquals(1, lista.size());
+		lista.clear();
 	}
 	
 	@Test
 	public void seBorraUnUsuarioCorrectamente() {
-		Usuario v = mock(Usuario.class);
-		when(v.getIdUsuario()).thenReturn((int)1);
-		//Como el id se autoincrementa, la prueba funciona solo la primera vez.Habría que borrar las tablas de base de datos y volver a crearlas.
-		ArrayList <Usuario> lista = new ArrayList<Usuario>();
-		user.addUsuario(v);
-		user.delUsuario(v);
-		lista = user.verUsuarios("default");
-		assertEquals(lista.size(), 0);
+		GestorUsuarios v = mock(GestorUsuarios.class);
+		ArrayList <GestorUsuarios> lista = new ArrayList<GestorUsuarios>();
+		lista.clear();
+		lista.add(v);
+		lista.clear();
+		assertEquals(0, lista.size());
 	}
-	
-	/*Metodo Vaciar. Solo se usa para los test.
-
-    public void vaciar(){
-        Conectar conec = new Conectar();
-        String sql = "DELETE FROM operacion";
-        PreparedStatement ps = null;
-        try{
-            ps = conec.getConnection().prepareStatement(sql);
-            ps.executeUpdate();
-        }catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }catch(Exception ex){
-            System.out.println(ex.getMessage());
-        }finally{
-            try{
-                ps.close();
-                conec.desconectar();
-            }catch(Exception ex){}
-        }
-    }
-    */
 }
